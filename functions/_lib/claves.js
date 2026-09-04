751874d187af0e9d2d5f72a9f74cf2193e8de0d1
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-04 11:07:15 -0500

    Usar consultas indexadas de títulos

--- functions/_lib/claves.js
+++ functions/_lib/claves.js
@@ -3,7 +3,7 @@
 import { text } from './firestore-fixed.js';
 import { generateWithProvider, listProviders, saveProvider, toggleProvider } from './ia-firebase.js';
 import { getStudentBasic, pullRequisitos } from './requisitos-firebase-fixed.js';
-import { executeTitulosAction, publicTitleConfiguration } from './titulos-firebase-v11.js';
+import { executeTitulosAction, publicTitleConfiguration } from './titulos-firebase-v12.js';
 
 const PRIVATE_STUDENT_FIELDS = new Set([
   'celular',
